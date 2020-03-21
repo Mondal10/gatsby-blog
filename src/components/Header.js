86a6e28b03613649c94b9841517220a5db7c6d61
Mondal10 <amitmondal111.am@gmail.com>
2020-03-21 23:38:25 +0530

    added sass plugin to gatsby-config, made moduled scss styles to scope it to particular component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+// Converts the '-' seperated class to camelCase class automatically 
+import { header, title, navList, navItem, activeNavItem } from '../styles/modules/header.module.scss';
+
 const Header = () => {
     return (
-        <header>
-            <h1>Amit Mondal</h1>
+        <header className={header}>
+            <h1>
+                <Link className={title} to="/">Amit Mondal</Link>
+            </h1>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                <ul className={navList}>
+                    {/* activeClassName sets css rule for the active Link(current page) clicked */}
+                    <li><Link className={navItem} activeClassName={activeNavItem} to="/">Home</Link></li>
+                    <li><Link className={navItem} activeClassName={activeNavItem} to="/blog">Blog</Link></li>
+                    <li><Link className={navItem} activeClassName={activeNavItem} to="/about">About</Link></li>
+                    <li><Link className={navItem} activeClassName={activeNavItem} to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,11 +3,16 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
+import '../styles/index.scss';
+import { container, content } from '../styles/modules/layout.module.scss';
+
 const Layout = (props) => {
     return (
-        <div>
-            <Header />
-            {props.children}
+        <div className={container}>
+            <div className={content}>
+                <Header />
+                {props.children}
+            </div>
             <Footer />
         </div>
     );
