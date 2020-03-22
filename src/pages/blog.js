e84ec9e2ca81930548764447af5340745c1605e6
Mondal10 <amitmondal111.am@gmail.com>
2020-03-22 16:25:24 +0530

    rendering the markdown

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/Layout';
 
 const BlogPage = () => {
+    const blogPosts = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title,
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
     return (
         <Layout>
             <h1>Blog</h1>
             <p>All Posts goes here</p>
+            <ol>
+                {
+                    blogPosts.allMarkdownRemark.edges.map(({ node }, i) => {
+                        return (
+                            <li key={i}>
+                                <h2>{node.frontmatter.title}</h2>
+                                <p>{node.frontmatter.date}</p>
+                            </li>
+                        );
+                    })
+                }
+            </ol>
         </Layout>
     );
 };
