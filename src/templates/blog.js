07bfa3f2ea44785d8a36851c3ac66d1d247efacb
Mondal10 <amitmondal111.am@gmail.com>
2020-03-25 20:55:00 +0530

    Rendering Post Data in Blog Template with graphql query

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 
-const Blog = () => {
-    return (
-        <Layout>
-            This is the Blog Template
-        </Layout>
-    )
+export const templateQuery = graphql`
+    query ($slug: String!){
+      markdownRemark(
+        fields:{
+          slug:{
+            eq: $slug
+          }
+        }
+      ){
+        frontmatter{
+          title,
+          date
+        }
+        html
+      }
+    }
+`;
+
+const Blog = (props) => {
+  // Nested Destructuring `props.data.markdownRemark`
+  const {
+    html,
+    frontmatter: { title, date }
+  } = props.data.markdownRemark;
+
+  return (
+    <Layout>
+      <h1>{title}</h1>
+      <p>{date}</p>
+      {/* dangerouslySetInnerHTML: https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml */}
+      <div dangerouslySetInnerHTML={{ __html: html }}></div>
+    </Layout>
+  )
 };
 
 export default Blog;
