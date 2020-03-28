48286450f8ea5ce69196430b649cce6c52d47421
Mondal10 <amitmondal111.am@gmail.com>
2020-03-28 21:35:28 +0530

    updating the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Layout from '../components/Layout';
+
 const NotFound = () => {
     return (
         <div>
-            <h1>No such page found</h1>
+            <Layout>
+                <h1>No such page found</h1>
+            </Layout>
         </div>
     );
 }
